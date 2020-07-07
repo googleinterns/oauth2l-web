e22943342f9b7f909424bec46221d57522022d25
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-07-07 18:42:02 -0500

    Finish frontend API token form (#5)
    * Create materialUI themed parent component
    
    * Remove comma, add styling, and add footer
    
    * Formatting
    
    * Remove logs
    
    * Change cloud logo font to white
    
    * Add formik wrapper
    
    * Formatting, add scopes, add next and back buttons
    
    * Styling, add loading indicator, add steps to form
    
    * Add Grid layout and remove some comments
    
    * Temporarily remove footer
    
    * Add Google cloud favicon
    
    * Remove images and extra CSS
    
    * Update document title and some css
    
    * Remove unused dependency
    
    * Update json compact naming
    
    * Rename layouts folder to layout
    
    * Fix name for tokenScopes
    
    * Add Yup validation
    
    * Fix validationSchema format
    
    * Add more styling
    
    * Remove some leftover files from transition
    
    * Formatting and linting
    
    * Add client CI
    
    * Add text input field for credential
    
    * Add alert and credential upload button
    
    * Add json validation
    
    * Add success message
    
    * Set form value upon credential upload
    
    * Finish credential upload page
    
    * Add validation for remaining forms
    
    * Update second step label based on first step
    
    * Update second step description and error
    
    * Fix error showing too early for token type
    
    * Formatting
    
    * Switch from scopes naming scheme to access
    
    * Formatting and linting
    
    * Minor changes
    
    * Test push
    
    * Undo change
    
    * Update credential form tick to match submission
    
    * Update error messages
    
    * Add backend files that were accidentally removed
    
    * Fix frontend tests

--- web/client/src/components/form/TokenAccess.jsx
+++ web/client/src/components/form/TokenAccess.jsx
@@ -2,25 +2,33 @@ import React from "react";
 import { Field } from "formik";
 import { Typography, Box } from "@material-ui/core";
 import { TextField } from "formik-material-ui";
+import PropTypes from "prop-types";
 import "../../styles/form.css";
 
 /**
+ * @param {Object} props contains field label to set form label
  * @return {Box} containing form fields for adding scopes
  */
-export default function TokenScopes() {
+export default function TokenAccess(props) {
+  const { label } = props;
+
   return (
     <Box className="form-box">
       <div className="form-text">
-        <Typography variant="h5">Enter scopes</Typography>
+        <Typography variant="h5">Enter {label.toLowerCase()}</Typography>
       </div>
       <Field
         multiline
         fullWidth
         variant="outlined"
-        label="Scopes"
-        name="tokenScopes"
+        label={label}
+        name={`token${label}`}
         component={TextField}
       />
     </Box>
   );
 }
+
+TokenAccess.propTypes = {
+  label: PropTypes.string,
+};
