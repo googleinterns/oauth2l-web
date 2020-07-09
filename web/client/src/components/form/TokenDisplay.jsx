4560aae8d18fcb202fd0ffd78343829fc2dd8f81
Leo Ruiz <lruiz0333@gmail.com>
2020-07-09 21:03:00 +0000

    fix lint issues

--- web/client/src/components/form/TokenDisplay.jsx
+++ web/client/src/components/form/TokenDisplay.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-import { Typography, Button,Grid, IconButton,Collapse} from "@material-ui/core";
+import {
+  Typography,
+  Button,
+  Grid,
+  IconButton,
+  Collapse,
+} from "@material-ui/core";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import PropTypes from "prop-types";
-import "../../styles/form.css"
-import {Alert} from '@material-ui/lab';
-import CloseIcon from '@material-ui/icons/Close'
+import "../../styles/form.css";
+import { Alert } from "@material-ui/lab";
+import CloseIcon from "@material-ui/icons/Close";
 /**
  *
- * @param {string} prop token being passed for display
+ * @param {string} props token being passed for display
  * @return {Grid} returns a Grid component that contains the page
  */
 export default function TokenDisplay(props) {
@@ -16,8 +22,8 @@ export default function TokenDisplay(props) {
   return (
     <Grid>
       <Typography variant="h5">Token:</Typography>
-      
-      <Typography noWrap variant="body1" className= "form-text" >
+
+      <Typography noWrap variant="body1" className="form-text">
         {token}
       </Typography>
       <CopyToClipboard text={token} onCopy={() => setCopy(true)}>
@@ -26,10 +32,23 @@ export default function TokenDisplay(props) {
         </Button>
       </CopyToClipboard>
 
-    <Collapse in = {copy}>
-      <Alert action = {<IconButton aria-label="close" color = "inherit" size = "small" onClick= {() => {
-          setCopy(!copy)
-      }}><CloseIcon fontSize = "inherit"/></IconButton>}>Successfully pasted the token to your clipboard!</Alert>
+      <Collapse in={copy}>
+        <Alert
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setCopy(!copy);
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+        >
+          Successfully pasted the token to your clipboard!
+        </Alert>
       </Collapse>
     </Grid>
   );
