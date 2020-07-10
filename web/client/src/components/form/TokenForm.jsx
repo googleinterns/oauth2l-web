caa225a167cceb93faa5181fa354a303108fc552
Leo Ruiz <lruiz0333@gmail.com>
2020-07-10 17:50:39 +0000

    update args to match backend

--- web/client/src/components/form/TokenForm.jsx
+++ web/client/src/components/form/TokenForm.jsx
@@ -67,7 +67,7 @@ export default function TokenForm(props) {
     if (typeof Response["error"] === undefined) {
       sendToken(Response["error"]);
     } else {
-      sendToken(Response["data"]["OAuth2lResponse"]);
+      sendToken(Response["data"]["oauth2lResponse"]);
     }
   }
   return (
