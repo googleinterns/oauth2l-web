7cd9a9db816074459f2be7d8fe6d9608d6c96a85
momolin78 <momolin78@gmail.com>
2020-07-13 20:51:40 -0400

    fixing merge conflict

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -28,19 +28,6 @@ function App() {
         <Grid container direction="column">
           <MaterialUI paperClass="paper-top">
             <ModuleInfo
-<<<<<<< HEAD
-              title="Getting a Token"
-              content={[
-                "This module is used to generate a OAuth access token. It is equivalent to using the fetch and the header OAuth2l request.",
-                "To obtain the token, must first choose from the form the type and format of the token, as well as enter the scopes they" +
-                  "want the token to be based on. Then, a credentials file must" +
-                  "be submitted, either as a JSON file or a JSON body",
-                "Once all the requirements are submitted, an access token will be" +
-                  "returned, based on the format requested.",
-              ]}
-              hasNote={true}
-              note="To obtain a JWT access token, a service account key must be included as the credentials file "
-=======
               title="Requesting a token"
               content={[
                 "This module is used to generate an OAuth access token. It is equivalent to using the fetch and header OAuth2l requests.",
@@ -49,7 +36,6 @@ function App() {
               ]}
               hasNote={true}
               note="To obtain a JWT access token, a service account key must be used as the credentials file"
->>>>>>> b13d5362d84d07040be49d54bf57dc968d3444cc
             />
             <Grid item className="main-content">
               <TokenForm parentCallback={callBackToken} />
@@ -69,19 +55,11 @@ function App() {
           <Grid item xs>
             <MaterialUI paperClass="paper-top">
               <ModuleInfo
-<<<<<<< HEAD
-                title="Validating Token"
-                content={[
-                  "This module is used for validating an OAuth access token and getting the info from a valid token. It is the equivalent of using the test and info OAuth2l command.",
-                  "When an OAuth access token is submitted, an icon will appear, stating whether the token is valid or not.",
-                  "If the token is valid, a information button will appear, which will display the info of the token when clicked.",
-=======
                 title="Validating your token"
                 content={[
                   "This module is used for validating an OAuth access token and getting the info from a valid token. It is the equivalent of using the test and info OAuth2l command.",
                   "When an OAuth access token is submitted, an icon will appear, stating whether the token is valid or not.",
                   "If the token is valid, an information button will appear, which will display the info of the token when clicked.",
->>>>>>> b13d5362d84d07040be49d54bf57dc968d3444cc
                 ]}
                 hasNote={true}
                 note="The info for a JWT Token cannot be requested"
