4af4495ac61cbabb528d9e8a7ab03dda01c7c5d9
momolin78 <momolin78@gmail.com>
2020-07-13 23:10:37 -0400

    resolving merge conflicts

--- web/client/src/components/index.js
+++ web/client/src/components/index.js
@@ -5,11 +5,8 @@ import TokenAccess from "./form/TokenAccess";
 import TokenCredentials from "./form/TokenCredentials";
 import TokenDisplay from "./form/TokenDisplay";
 import ValidateToken from "./validation/TokenValidation";
-<<<<<<< HEAD
 import ReqModule from "./request/ReqModule";
-=======
 import ModuleInfo from "./info/ModuleInfo";
->>>>>>> b13d5362d84d07040be49d54bf57dc968d3444cc
 export {
   Head,
   TokenForm,
@@ -18,9 +15,6 @@ export {
   TokenCredentials,
   TokenDisplay,
   ValidateToken,
-<<<<<<< HEAD
   ReqModule,
-=======
   ModuleInfo,
->>>>>>> b13d5362d84d07040be49d54bf57dc968d3444cc
 };
