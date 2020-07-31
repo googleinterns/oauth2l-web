0123cc701abeb14cbd8237e266018ba597f2af7d
Melissa Lin <momolin78@gmail.com>
2020-07-31 02:33:14 +0000

    Frontend module info (#46)
    * adding info button that contains dialog box describing the function of each module
    
    * removing unecessary id names
    
    * changing the dialog box handlers from const to functions. Also fixing test changes
    
    * adhering to the google JS style guide
    
    * added notes to module dialog content
    
    * Change wording in a few places and add bottom margin to text
    
    * fixing merge conflict
    
    * changed text font of dialog content
    
    * temp commit
    
    * fixing the regex so that webapp will be supposed on all browser. Also removing unecessary logic checks
    
    * reverting package name
    
    * adding welcome box to webapp
    
    * adding links to module info box
    
    * adding box that appears on application render and updated token form module with info about caching a token
    
    * Remove oauth2l base
    
    * Update workflows
    
    * Add go.mod and go.sum
    
    * Remove hidden files
    
    * adding go.mod and go.sum
    
    * fixing test by removing root go.mod and go.sum
    
    * fixing tests
    
    * utilizing localstorage so that popup will only show at the first render
    
    * enlarging text
    
    * Revert "Merge branch 'master' into frontend-module-info"
    
    This reverts commit 5f3f7673e950d769b632d6229c24aaf3a19f213b, reversing
    changes made to b54fc59a050fd10805954818dd6fc3387ad098eb.
    
    * removing comments
    
    * adding back token display
    
    * fixing react hooks
    
    * fixing react hooks
    
    * fixing react hooks
    
    * fixing react hooks
    
    * fixing merge conflicts
    
    Co-authored-by: Yousef Ahmed <yousefahmed0066@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -16,9 +16,8 @@ import "./styles/app.css";
  * @return {MaterialUI} themed app
  */
 function App() {
-  const [token, setToken] = useState("");
-  const [responseVisable, setResponseVisable] = useState(false);
-
+  const [response, setResponse] = useState(null); // holds the response from the backend
+  const [tokenResponseVisable, setTokenResponseVisable] = useState(true); // the state that decides which component to hide/show
   const [httpResponse, setHttpResponse] = useState("");
   const [httpresponseVisable, setHttpResponseVisable] = useState(false);
   /**
@@ -26,12 +25,20 @@ function App() {
    * @param {string} childData obtains the token value from the TokenForm component
    */
   const callBackToken = (childData) => {
-    setToken(childData);
-    setResponseVisable(true);
+    setResponse(childData);
+    setTokenResponseVisable(false);
   };
 
   /**
    *
+   * @param {bool} visibility obtains the bool to render the form back and if clicked it resets to the last component
+   */
+
+  const backClicked = (visibility) => {
+    setTokenResponseVisable(!visibility);
+  };
+
+  /**
    * @param {string} childData holds the response of the HTTP request.
    * displaying the page with the HTTP response.
    */
@@ -105,6 +112,69 @@ function App() {
                 </div>
               </Grid>
             </MaterialUI>
+          </Grid>
+        </Grid>
+        <Grid item xs>
+          <Grid container direction="column">
+            <Grid item xs>
+              <MaterialUI paperClass="paper-top">
+                <ModuleInfo
+                  title="Validating your token"
+                  content={[
+                    "This module is used for validating an OAuth access token and getting the info from a valid token. It is the equivalent of using the test and info OAuth2l command.",
+                    "When an OAuth access token is submitted, an icon will appear, stating whether the token is valid or not.",
+                    "If the token is valid, an information button will appear, which will display the info of the token when clicked.",
+                  ]}
+                  hasNote={true}
+                  note={["The info for a JWT Token cannot be requested."]}
+                  hasLinks={false}
+                  links={[]}
+                />
+                <Grid item xs className="main-content">
+                  <ValidateToken />
+                </Grid>
+              </MaterialUI>
+            </Grid>
+            <Grid item xs>
+              <MaterialUI paperClass="paper-bottom">
+                <ModuleInfo
+                  title="Use your token for an HTTP request"
+                  content={[
+                    "This module is used for using a OAuth2l access token to make a HTTP request. It is the equivalent of using the curl OAuth2l command.",
+                    "To make the request, you must submit the URL to the request, the method of the request, and the token you will be using in the request. You also have the option of entering a request body, headers, and content type of the reponse.",
+                    "Once that is all submitted, the request will be made and the response will be displayed.",
+                  ]}
+                  hasNote={false}
+                  note={[]}
+                  hasLinks={true}
+                  links={[
+                    {
+                      name: "Sending an Access Token to an API.",
+                      url:
+                        "https://developers.google.com/identity/protocols/oauth2#4.-send-the-access-token-to-an-api.",
+                    },
+                  ]}
+                />
+                <Grid item xs className="main-content">
+                  {/* <RequestModule /> */}
+                  {!httpresponseVisable ? (
+                    <RequestModule
+                      parentCallback={(childData) =>
+                        callBackHttpResponse(childData)
+                      }
+                    />
+                  ) : (
+                    <RequestDisplay
+                      httpResponse={httpResponse}
+                      parentCallback={(httpresponseVisable) =>
+                        resetHttpRequest(httpresponseVisable)
+                      }
+                      responseVisable={httpresponseVisable}
+                    />
+                  )}
+                </Grid>
+              </MaterialUI>
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs>
