d7d73c197e8438ff459db24530f6e98d414adbc7
Melissa Lin <momolin78@gmail.com>
2020-08-04 17:10:01 +0000

    Add binary script (#45)
    * adding python script to download oauth2l binary
    
    * moving python script to api
    
    * deleting duplicate python script
    
    * adding setup to test files and backend to download binaries
    
    * fixing tests
    
    * updating gitignore to ignore the installed binaries
    
    * fixing gitignore
    
    * fixing gitignore
    
    * fixing gitignore
    
    * fixing gitignore
    
    * fixing gitignore
    
    * fixing gitignore
    
    * adding spaces between operators in python script
    
    * removing binaries file

---
+++ api/get_binaries.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import os
+import platform
+
+def main():
+  binaryURL = ""
+  if platform.system() == "Linux":
+    binaryURL = "https://storage.googleapis.com/oauth2l/latest/linux_amd64.tgz"
+  elif platform.system() == "Darwin":
+    binaryURL = "https://storage.googleapis.com/oauth2l/latest/darwin_amd64.tgz"
+  elif platform.system() == "Windows":
+    binaryURL = "https://storage.googleapis.com/oauth2l/latest/windows_amd64.tgz"
+  else:
+    print("Platform not supported.")
+    return
+    
+  os.system("mkdir binaries| wget " + binaryURL + " -O - | tar -xz -C binaries --strip-components=1")
+
+if __name__ == '__main__':
+  main()
