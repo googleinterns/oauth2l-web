1806b760309fbc16db925acc08365db4cc279047
Yousef Ahmed <yousefahmed0066@gmail.com>
2020-08-07 11:09:54 -0500

    Restrict unwanted methods in the backend

--- api/backend.go
+++ api/backend.go
@@ -52,7 +52,7 @@ const tokenExpiryMinutes = 10
 // SetupResponseHeaders sets up the headers for the response.
 func setupResponseHeaders(w *http.ResponseWriter, req *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
 
